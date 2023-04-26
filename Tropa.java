8203d9908f62c719ce25f7d7e13fca6c69be8621
Jexoncr <jexonsc@gmail.com>
2023-04-25 20:39:40 -0600

    Clase Tropa
    El constructor inicializa algunas de estas variables de acuerdo con los parámetros que se pasan a él. La velocidad es controlada por un parámetro adicional 'direccion'. El tipo de tropa también es utilizado para establecer la debilidad y el daño.

--- Tropa.java
+++ Tropa.java
@@ -35,4 +35,82 @@ public class Tropa {
             daño = 1;
         }
         
-    } }
+    }
+
+    public double getDaño() {
+        return daño;
+    }
+    
+    public void setIncognito(){
+        if (esperar>(2*30)){
+            img="incognito";
+        }
+    }
+
+    public boolean chocaCastillo(Castillo castillo) {
+        if (this.getX() + this.getWidth() >= castillo.getX() && this.getX() <= castillo.getX() + castillo.getWidth()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean chocaTropa(Tropa enemigo) {
+        if (this.getX() + this.getWidth() >= enemigo.getX()
+                && this.getX() <= enemigo.getX() + enemigo.getWidth()
+                && this.getY() + this.getHeight() >= enemigo.getY()
+                && this.getY() <= enemigo.getY() + enemigo.getHeight()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean esDebilidad(String tip) {
+        return debilidad.equals(tip);
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getDebilidad() {
+        return debilidad;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void draw(Graphics g) {
+        Image imge = Toolkit.getDefaultToolkit().getImage("src/main/java/"+img+".png");
+        g.drawImage(imge, x, y, width, height, null);
+    }
+
+    public void move() {
+        if (esperar == 0) {
+            x += speed;
+        } else {
+            esperar--;
+        }
+    }
+
+    public void setPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
